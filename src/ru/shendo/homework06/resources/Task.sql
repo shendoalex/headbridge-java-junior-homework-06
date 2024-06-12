0549813c9d3371ffaf7c11fdd3472e8b43f3078d
Aleksei Shendo <shendo.alex@gmail.com>
2024-06-12 19:12:41 +0300

    Homework06 - done

---
+++ src/ru/shendo/homework06/resources/Task.sql
@@ -0,0 +1,118 @@
+-- Задание: 1
+-- Найдите номер модели, скорость и размер жесткого диска для всех ПК стоимостью менее 500 дол.
+-- Вывести: model, speed и hd
+
+SELECT model, speed, hd
+FROM PC
+WHERE price < 500;
+
+--
+
+-- Задание: 2
+-- Найдите производителей принтеров.
+-- Вывести: maker
+
+SELECT DISTINCT maker
+FROM Product
+WHERE type = 'Printer';
+
+--
+
+-- Задание: 3
+-- Найдите номер модели, объем памяти и размеры экранов ПК-блокнотов,
+-- цена которых превышает 1000 дол.
+
+SELECT model, ram, screen
+FROM Laptop
+WHERE price > 1000;
+
+--
+
+-- Задание: 4
+-- Найдите все записи таблицы Printer для цветных принтеров.
+
+SELECT *
+FROM Printer
+WHERE color = 'y';
+
+--
+
+-- Задание: 5
+-- Найдите номер модели, скорость и размер жесткого диска ПК,
+-- имеющих 12x или 24x CD и цену менее 600 дол.
+
+SELECT model, speed, hd
+FROM PC
+WHERE cd IN ('12x', '24x')
+  AND price < 600;
+
+--
+
+-- Задание: 6
+-- Для каждого производителя, выпускающего ПК-блокноты
+-- c объёмом жесткого диска не менее 10 Гбайт, найти скорости таких ПК-блокнотов.
+-- Вывод: производитель, скорость.
+
+SELECT DISTINCT maker, speed
+FROM Laptop as l,
+     Product as p
+WHERE l.model = p.model
+  AND l.hd >= 10;
+
+--
+
+-- Задание: 7
+-- Найдите номера моделей и цены всех имеющихся в продаже продуктов (любого типа)
+-- производителя B (латинская буква).
+
+SELECT prod.model, price
+FROM Product prod,
+     PC pc
+WHERE prod.model = pc.model
+  AND maker = 'B'
+UNION
+SELECT prod.model, price
+FROM Product prod,
+     Printer pr
+WHERE prod.model = pr.model
+  AND maker = 'B'
+UNION
+SELECT prod.model, price
+FROM Product prod,
+     Laptop lp
+WHERE prod.model = lp.model
+  AND maker = 'B';
+
+--
+
+-- Задание: 8
+-- Найдите производителя, выпускающего ПК, но не ПК-блокноты.
+
+SELECT DISTINCT maker
+FROM Product
+WHERE Product.type = 'PC'
+  AND Product.maker
+    NOT IN (SELECT Product.maker
+            FROM Product
+            WHERE Product.type = 'Laptop');
+
+--
+
+-- Задание: 9
+-- Найдите производителей ПК с процессором не менее 450 Мгц.
+-- Вывести: Maker
+
+SELECT DISTINCT maker
+FROM Product,
+     PC
+WHERE Product.model = PC.model AND PC.speed >= 450;
+
+--
+
+-- Задание: 10
+-- Найдите модели принтеров, имеющих самую высокую цену.
+-- Вывести: model, price
+
+SELECT model, price
+FROM Printer
+WHERE price = (SELECT MAX(price) FROM Printer);
